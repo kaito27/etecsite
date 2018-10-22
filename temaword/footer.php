042554d60d1437a95a4fc60b85a737eb25e651aa
Caio Vinicius <caio27vinicius@gmail.com>
2018-10-22 21:28:16 +0000

    alteraçao footer

--- temaword/footer.php
+++ temaword/footer.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-md-12 rodape" align="center">
-        <p>Site desenvolvido por CLL</p>
+        <p>Site desenvolvido por Caio, Lucas E e Lucas P</p>
     </div>
 </div>
 <?php wp_footer();?>
